7006573f1d96d2156d0fa12af85dc7aec86110e1
ameya-6964 <belvalkar7@gmail.com>
2023-10-15 00:07:31 +0530

    Completed Uniion Type

---
+++ dist/Objects.js
@@ -0,0 +1,8 @@
+"use strict";
+let employee = {
+    id: 1,
+    name: "Ameya",
+    retire: (date) => {
+        console.log(date);
+    },
+};
---
+++ dist/TypeAlias.js
@@ -0,0 +1,8 @@
+"use strict";
+let employee1 = {
+    id: 1,
+    name: "Ameya",
+    retire: (date) => {
+        console.log(date);
+    },
+};
