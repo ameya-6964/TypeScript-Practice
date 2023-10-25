2a3b89abefd5947dcab017db03fc758eba3359fc
ameya-6964 <belvalkar7@gmail.com>
2023-10-25 16:48:46 +0530

    Added Code For Functions

--- dist/Functions.js
+++ dist/Functions.js
@@ -1,8 +1,17 @@
 "use strict";
-function calculateTax(income, taxYear = 2022) {
-    if (income > 50000 && taxYear < 2022) {
-        return income * 1.2;
-    }
-    return income * 0.5;
+function greet(name) {
+    return `Hello ${name}, Nice To See You`;
 }
-calculateTax(102000);
+let greet1 = greet("Ameya");
+console.log(greet1);
+function isEligble(name, age) {
+    let eligibleAge = 18;
+    if (age >= 18) {
+        return `You Are Eligible ${name} Your Age Is ${age} Years 😀`;
+    }
+    return `You Are Not Eligible ${name} Please Apply After ${eligibleAge - age} Years 😇`;
+}
+let driver1 = isEligble("Ameya", 18);
+console.log(driver1);
+let driver2 = isEligble("Jayesh", 16);
+console.log(driver2);
--- src/Functions.ts
+++ src/Functions.ts
@@ -1,4 +1,4 @@
-function calculateTax(income: number, taxYear: number = 2022): number {
+/* function calculateTax(income: number, taxYear: number = 2022): number {
   if (income > 50_000 && taxYear < 2022) {
     return income * 1.2;
   }
@@ -6,3 +6,27 @@ function calculateTax(income: number, taxYear: number = 2022): number {
 }
 
 calculateTax(10_2000);
+
+
+ */
+
+function greet(name: string): string {
+  return `Hello ${name}, Nice To See You`;
+}
+let greet1 = greet("Ameya");
+console.log(greet1);
+
+function isEligble(name: string, age: number): string {
+  let eligibleAge = 18;
+  if (age >= 18) {
+    return `You Are Eligible ${name} Your Age Is ${age} Years 😀`;
+  }
+  return `You Are Not Eligible ${name} Please Apply After ${
+    eligibleAge - age
+  } Years 😇`;
+}
+
+let driver1 = isEligble("Ameya", 18);
+console.log(driver1);
+let driver2 = isEligble("Jayesh", 16);
+console.log(driver2);
