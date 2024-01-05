6bd105a9e2be2bf7aa531946fe544ab46c1e798e
ameya-6964 <belvalkar7@gmail.com>
2024-01-05 18:46:36 +0530

    Typo Fixed

--- Professional-TypeScript-Course/Foundation/dist/vanilla.js
+++ Professional-TypeScript-Course/Foundation/dist/vanilla.js
@@ -1,11 +1,18 @@
 const num1 = document.getElementById("num1");
 const num2 = document.getElementById("num2");
 const button = document.getElementById("btn");
+const result = document.getElementById("result");
 
 function add(num1, num2) {
   return num1 + num2;
 }
 
 button.addEventListener("click", () => {
-  console.log(add(num1.value, num2.value));
+  const value = ` Addition Of ${num1.value} and ${num2.value} Is ${add(
+    num1.value,
+    num2.value
+  )}`;
+  num1.value = "";
+  num2.value = "";
+  result.textContent = value;
 });
