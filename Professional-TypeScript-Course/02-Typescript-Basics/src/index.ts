2a1b76650e717fd35fd7003752ea673b1b35afdd
ameya-6964 <belvalkar7@gmail.com>
2024-01-05 21:37:35 +0530

    Added Primitive Datatypes

---
+++ Professional-TypeScript-Course/02-Typescript-Basics/src/index.ts
@@ -0,0 +1,12 @@
+// Core Primitive Types In JavaScript'
+
+//* Number Datatype
+let a: number = 23;
+let b: number = 2.3;
+let c: number = -2.3;
+
+//* String Datatype
+let e: string = "Hello World";
+
+//* Boolean DataType
+let f: boolean = true;
