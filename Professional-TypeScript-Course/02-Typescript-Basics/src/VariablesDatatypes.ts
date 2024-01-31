2e09c22b111be8a64a479632d460aed0608733a9
ameya-6964 <belvalkar7@gmail.com>
2024-02-01 00:22:54 +0530

    Added Octal Binary Hexadecimal Number Example

---
+++ Professional-TypeScript-Course/02-Typescript-Basics/src/VariablesDatatypes.ts
@@ -0,0 +1,18 @@
+// Core Primitive Types In JavaScript'
+
+//* Number Datatype
+let a: number = 23;
+let b: number = 2.3;
+let c: number = -2.3;
+//Octal Number
+let d: number = 0o270; //184
+//Binary Number
+let e: number = 0b111010; //58
+//Hexadecimal Number
+let h: number = 0x27ad; // 10157
+
+//* String Datatype
+let f: string = "Hello World";
+
+//* Boolean DataType
+let g: boolean = true;
