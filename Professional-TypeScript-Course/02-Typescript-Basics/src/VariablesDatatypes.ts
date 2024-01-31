25e47f03b7cbaa547af7d1b2d169695a31d0332a
ameya-6964 <belvalkar7@gmail.com>
2024-02-01 00:34:09 +0530

    added undefined and Null Type Primitive Values

--- Professional-TypeScript-Course/02-Typescript-Basics/src/VariablesDatatypes.ts
+++ Professional-TypeScript-Course/02-Typescript-Basics/src/VariablesDatatypes.ts
@@ -17,3 +17,12 @@ let f: string = "Hello World";
 //* Boolean DataType
 let g: boolean = true;
 let i: boolean = false;
+
+//* undefined And Null Type
+
+let k: null = null;
+let l: number = 5;
+let u: undefined;
+
+console.log(u + l); // NaN -> Not a number -> undefined + 5 -> Nan
+console.log(k + 5); // 5 -> as k is null and any primitive operation On It Will Be Treated As 0
